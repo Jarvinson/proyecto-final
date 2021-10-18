44c05355a559437a112ef2d4be86f67af4c78bfb
Juan Montoya <you@example.com>
2021-10-17 23:06:41 -0500

    pruebas unitarias a detalle_compra

--- persistencia/src/main/java/co/edu/uniquindio/proyecto/repositorios/DetalleCompraRepo.java
+++ persistencia/src/main/java/co/edu/uniquindio/proyecto/repositorios/DetalleCompraRepo.java
@@ -1,15 +1,13 @@
 package co.edu.uniquindio.proyecto.repositorios;
-import co.edu.uniquindio.proyecto.entidades.Mensaje;
+
+import co.edu.uniquindio.proyecto.entidades.DetalleCompra;
+import co.edu.uniquindio.proyecto.entidades.Subasta;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
-public interface MensajeRepo extends JpaRepository<Mensaje, Integer> {
-
-
-    Page<Mensaje> findAll(Pageable paginador);
+public interface DetalleCompraRepo extends JpaRepository<DetalleCompra, Integer> {
+    Page<DetalleCompra> findAll(Pageable paginador);
 }
